cb700d277a989786aaf0abfb071a574bb0745842
jungeun826 <jungeun826@nate.com>
2014-01-06 15:15:32 +0900

    153페이지 실습 3.3.8
    이미지 기반의 pickerView 실습을 완료했다.

--- ImageBasedPickerVIew/ViewController.m
+++ ImageBasedPickerVIew/ViewController.m
@@ -7,13 +7,41 @@
 //
 
 #import "ViewController.h"
+#define MAX_NUM 6
+@interface ViewController () <UIPickerViewDataSource, UIPickerViewDelegate>
 
-@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 
 @end
 
 @implementation ViewController
-
+- (IBAction)selectRandom:(id)sender {
+    int r = arc4random() % MAX_NUM;
+    [self.picker selectRow:r inComponent:0 animated:YES];
+}
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    return MAX_NUM;
+}
+-(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component{
+    return 64;
+}
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
+    NSString *imagePath = [NSString stringWithFormat:@"snsd%d.png",(int)row+1];
+    UIImage *image = [UIImage imageNamed:imagePath];
+    UIImageView *imageView;
+    
+    if(view == nil){
+        imageView = [[UIImageView alloc]initWithImage:image];
+        imageView.frame = CGRectMake(0, 0, 100, 60);
+    }else{
+        imageView = (UIImageView *)view;
+        imageView.image = image;
+    }
+    return imageView;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
